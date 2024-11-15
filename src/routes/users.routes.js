d36a5edae3887d5b385e83c0b8bfefcfb3dd1706
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 21:05:12 -0500

    Se agrega la ruta para obtener por username

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,8 @@ import { methods as usersController } from "../controllers/users.controller";
 const router = Router();
 
 // Routes
-router.get("/", usersController.getUsers);
+router.get("/", usersController.getUsers); // Find All
+router.get("/:username", usersController.getUsersById); // Find By Id
 
 
 export default router;
