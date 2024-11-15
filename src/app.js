2e774467eb4ff7c4d2f1d9b7623c3bc01058e873
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 20:49:48 -0500

    Se importa el express para poder interpretar Json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+import express from "express";
 import morgan from "morgan";
 
 // Routes
+import usersRoutes from "./routes/users.routes";
 
 const app = express();
 
@@ -9,7 +11,9 @@ app.set("port", 3312);
 
 // Middelwares (funciones entre una peticion y una respuesta)
 app.use(morgan("dev"));
+app.use(express.json());
 
 // Routes
+app.use("/api/users", usersRoutes);
 
 export default app;
