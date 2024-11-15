6a404b995f44d9ea9ee543b3f13d1dd06d678c2c
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 21:40:35 -0500

    Se establece ruta para funcion eliminar usuario

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,6 +8,6 @@ router.get("/", usersController.getUsers); // Find All
 router.get("/:id", usersController.getUserById); // Find By Id
 router.post("/", usersController.addUser); // Add user
 router.put("/:id", usersController.updateUser); // Edit user
-
+router.delete("/:id", usersController.deleteUser); // Delete user
 
 export default router;
