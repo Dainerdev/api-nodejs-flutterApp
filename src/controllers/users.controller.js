d950fc36a09e0f90f086343c2d65d7e1f217a135
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 21:04:37 -0500

    Se desarrolla funcion para obtener usuarios por username (identificación)

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -11,6 +11,21 @@ const getUsers = async (req, res) => {
     }
 };
 
+const getUsersById = async (req, res) => {
+    try {
+        console.log(req.params);
+        const { username } = req.params;
+
+        const connection = await getConnection();
+        const result = await connection.query("SELECT * FROM users WHERE username = ?", username);
+        res.json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+
 export const methods = {
-    getUsers
+    getUsers,
+    getUsersById
 }
