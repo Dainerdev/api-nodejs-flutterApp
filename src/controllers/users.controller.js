9382dabc1e6c33d023477c93df6621efadee7999
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 21:33:27 -0500

    Se desarrolla funcion para editar datos de usuario

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,6 @@
 import { getConnection } from "./../database/database";
 
-// Get Users
+// GET Users
 const getUsers = async (req, res) => {
     try {
         const connection = await getConnection();
@@ -11,20 +11,20 @@ const getUsers = async (req, res) => {
     }
 };
 
-//Get Users By Id
+// GET Users By Id
 const getUsersById = async (req, res) => {
     try {
-        const { username } = req.params;
+        const { id } = req.params;
 
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM users WHERE username = ?", username);
+        const result = await connection.query("SELECT * FROM users WHERE id = ?", id);
         res.json(result);
     } catch (error) {
         res.status(500).send(error.message);
     }
 };
 
-// Add users
+// POST users
 const addUsers = async (req, res) => {
     try {
         const { username, email, password, names, lastNames, country, city } = req.body;
@@ -45,8 +45,32 @@ const addUsers = async (req, res) => {
     }
 };
 
+// UPDATE users
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const { username, email, password, names, lastNames, country, city } = req.body;
+
+        // Validation
+        if (email === undefined || password === undefined || names === undefined || 
+            lastNames === undefined || country === undefined || city === undefined) {
+            res.status(400).json({ message: "Bad request. Please fill all fields." });
+        }
+
+        const user = { id, username, email, password, names, lastNames, country, city };
+
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE users SET ? WHERE id = ?",[user, id]);
+        res.json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
 export const methods = {
     getUsers,
     getUsersById,
-    addUsers
+    addUsers,
+    updateUser
 }
